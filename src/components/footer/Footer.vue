728da328bf5ec1378a8d6c402e83077933ce88cb
Damian-radzio <drado2003@gmail.com>
2022-06-17 09:57:12 +0200

    added menu and footer components

---
+++ src/components/footer/Footer.vue
@@ -0,0 +1,87 @@
+<template>
+  <footer>
+    <section class="facebook-container">
+      <h2>Facebook</h2>
+      <Facebook />
+    </section>
+    <section>
+      <h2>Instagram</h2>
+      <div class="photos">
+        <div class="photo"></div>
+        <div class="photo"></div>
+        <div class="photo"></div>
+        <div class="photo"></div>
+        <div class="photo"></div>
+        <div class="photo"></div>
+      </div>
+    </section>
+    <section class="contact">
+      <h2>Kontakt</h2>
+      <div class="contact-data-container">
+        <p>(+48) 555 555 555</p>
+        <p>djslawekdziadek@gmail.com</p>
+        <p>formularz kontaktowy</p>
+      </div>
+    </section>
+  </footer>
+</template>
+
+<style lang="scss">
+footer {
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 85px;
+  section {
+    text-align: center;
+    width: 30%;
+    height: 250px;
+    h2 {
+      margin-bottom: 20px;
+    }
+  }
+  .facebook-container {
+    svg {
+      margin-top: 40px;
+    }
+  }
+  .photos {
+    display: flex;
+    max-width: 310px;
+    flex-wrap: wrap;
+    margin: 0 auto;
+    text-align: center;
+    justify-content: space-between;
+    align-items: space-around;
+    height: 210px;
+    margin-top: 40px;
+    .photo {
+      width: 100px;
+      height: 100px;
+      background-color: grey;
+    }
+  }
+  .contact {
+    display: flex;
+    flex-direction: column;
+    .contact-data-container {
+      margin-top: 35px;
+      padding-left: 150px;
+      p {
+        padding-bottom: 25px;
+        max-width: 250px;
+        text-align: left;
+      }
+    }
+  }
+}
+</style>
+
+<script>
+import Facebook from '../../assets/svg/facebook.svg';
+export default {
+  name: 'Menu',
+  components: { Facebook },
+};
+</script>
---
+++ src/components/menu/Menu.vue
@@ -0,0 +1,66 @@
+<template>
+  <header class="header-container">
+    <div class="icon-wrapper">
+      <DjDziadekIcon />
+    </div>
+    <nav>
+      <ul>
+        <li><router-link class-active="active" to="/">Start</router-link></li>
+        <li><router-link class-active="active" to="/about">O mnie</router-link></li>
+        <li><router-link class-active="active" to="/gallery">Galeria</router-link></li>
+        <li><router-link class-active="active" to="/reviews">Opinie</router-link></li>
+        <li><router-link class-active="active" to="/contact">Kontakt</router-link></li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<style lang="scss">
+.header-container {
+  display: flex;
+  align-items: center;
+  display: flex;
+
+  justify-content: space-between;
+  .icon-wrapper {
+    margin-left: 100px;
+  }
+  nav {
+    margin-right: 100px;
+    width: 50%;
+    min-width: 700px;
+    ul {
+      width: 100%;
+      display: flex;
+      width: 100%;
+      max-width: 1200px;
+      justify-content: space-between;
+
+      li {
+        list-style: none;
+        padding: 5px 15px;
+        min-width: 100px;
+        text-align: center;
+        a {
+          font-weight: 600;
+          font-size: 20px;
+          text-decoration: none;
+          color: #1d1d1d;
+          font-weight: 400;
+          &.router-link-exact-active {
+            font-weight: 600;
+          }
+        }
+      }
+    }
+  }
+}
+</style>
+
+<script>
+import DjDziadekIcon from '../../assets/svg/dj-dziadek-logo.svg';
+export default {
+  name: 'Menu',
+  components: { DjDziadekIcon },
+};
+</script>
