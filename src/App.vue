8fbf6461ff707adb99b3598c461f3c4772b770f4
Damian-radzio <drado2003@gmail.com>
2022-06-17 09:57:21 +0200

    added src folder

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body {
+  font-family: 'Brygada 1918', serif;
+  position: relative;
+  min-height: 220vh;
+}
+
+#footer {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  height: 2.5rem;
+  z-index: -100;
+}
+</style>
+
+<template>
+  <Menu />
+  <router-view />
+  <div id="footer">
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Menu from './components/menu/Menu.vue';
+import Footer from './components/footer/Footer.vue';
+export default {
+  name: 'App',
+  components: {
+    Menu,
+    Footer,
+  },
+};
+</script>
